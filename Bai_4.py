0f2a4f2dd654f34b87e9915870b1fe02048ed7e6
nguyen-1610 <duynguyenhuynh250@gmail.com>
2025-10-17 16:38:48 +0700

    sửa b4

---
+++ Bai_4.py
@@ -0,0 +1,10 @@
+filename = input("Enter file name: ")
+
+try:
+    with open(filename, 'r', encoding='utf-8') as f:
+        content = f.read()
+        print("\nFile content:")
+        print(content)
+
+except FileNotFoundError:
+    print("Error: File không tồn tại!")
